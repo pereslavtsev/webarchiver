32dcbf7429b43775a1d1a18ccfb95960c82aec23
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2023-09-14 20:41:52 +0300

    chore(app): grpc config

--- packages/server/src/core/config/grpc.config.ts
+++ packages/server/src/core/config/grpc.config.ts
@@ -2,4 +2,5 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('grpc', () => ({
   enabled: true,
+  port: 50051,
 }));
--- packages/server/src/core/services/grpc-config.service.ts
+++ packages/server/src/core/services/grpc-config.service.ts
@@ -1,15 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
 import path from 'path';
+import grpcConfig from '../config/grpc.config';
+import { ConfigType } from '@nestjs/config';
 
 @Injectable()
 export class GrpcConfigService {
+  constructor(
+    @Inject(grpcConfig.KEY)
+    private readonly config: ConfigType<typeof grpcConfig>,
+  ) {}
+
   createGrpcOptions(): GrpcOptions | Promise<GrpcOptions> {
+    const { port } = this.config;
     return {
       transport: Transport.GRPC,
       options: {
         package: 'webarchiver',
         protoPath: path.join(__dirname, '../../archiver/archiver.proto'),
+        url: `0.0.0.0:${port}`,
       },
     };
   }
